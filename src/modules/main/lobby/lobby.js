d55bcbce5bb9de2d3917148b7a7d7c8ecb10075e
Sebastian Schwarz <12232063+itsmebasti@users.noreply.github.com>
2022-05-04 14:36:31 +0200

    switched default to tetris

--- src/modules/main/lobby/lobby.js
+++ src/modules/main/lobby/lobby.js
@@ -2,8 +2,8 @@ import { LightningElement, track } from 'lwc';
 
 export default class Lobby extends LightningElement {
     @track games = {
-        tetris: 0,
-        snake: 1,
+        tetris: 1,
+        snake: 0,
         painter: 0,
     }
     
